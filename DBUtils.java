d234f761fd86c98a67ebe507452c2bb3a6bb6a04
haribodeddula <hari.bodeddula@gmail.com>
2022-02-23 22:34:28 +0530

    updates as well as new file cleanup.sh

--- DBUtils.java
+++ DBUtils.java
@@ -1,1 +1,25 @@
 This is the first git file
+here is the update performed on the java file
+
+cd /var/www/html/index.html
+have to practise all the tags --skip_tags and how to run specifc tag
+roles
+-default
+-meta - to initialize anything before role get start
+-var
+-tasks
+-handlers
+-templates
+
+ansible_valut for maintaining secret passwords and how to invoke the dynamic inventory
+nd connect to ec2 instance and get the hostnames and install tomcat in all the instances.
+
+---
+- hosts: all
+  become:yes
+  gather_facts: true
+  remote_user: root
+  tasks:
+   - name: "createing the user and proving the password authentication and nopassword in /etc/sshd/*conf file)"
+     user:
+
---
+++ cleanup.sh
@@ -0,0 +1,6 @@
+this is the clean up script
+find command
+sed -n '7,8p' filename
+sed -n '7,8d' filename
+
+find . -type f testing.sh 
